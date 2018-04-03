306b663976c5762778810604c3e931d69a9bb01e
utkarshSINGHrathor <uutkarshsingh@gmail.com>
2018-04-04 02:48:28 +0530

    Initial Commit

---
+++ DataStructures/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructures
+//
+//  Created by Utkarsh Rathor on 04/04/18.
+//  Copyright © 2018 RazeWare. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
